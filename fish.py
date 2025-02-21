b3d3cbeca5ae4895e7e1069087225b29c499ee75
derekhlindell <derekhlindell@gmail.com>
2025-02-20 20:57:45 -0800

    added in existing data from import and new boilerplate format in fish.py

---
+++ fish.py
@@ -0,0 +1,14 @@
+fish = {
+           "anchovy" = {
+               "name":                  "anchovy",
+               "sell":                  200,
+               "where":                 "sea",
+               "shadow":                "small", 
+               "catch_difficulty":      "very_easy", 
+               "vision":                "very_wide", 
+               "catches_to_unlock":     0,
+               "months": {
+                   NH:[jan,feb,mar,apr,may,jun,july,aug,sep,oct,nov,dec], 
+                   SH:[jan,feb,mar,apr,may,jun,july,aug,sep,oct,nov,dec]
+           },
+       }
